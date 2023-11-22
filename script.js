f163d87e8d0b568a8b6af891245a7fe0cde90fff
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-22 09:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const formElement = document.querySelector("#MyForm");
 function volume_sphere() {
     //Write your code here
-  
+	const radius = Number(formElement.radius.value);
+	const volume = (4 * Math.PI * radius**3) / 3.0;
+	// formElement.volume.value= String(volume).slice(0, 4);
+	formElement.volume.value= volume;
+		
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
