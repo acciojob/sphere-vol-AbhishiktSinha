e0b716ccd7480ea24780dec07e0e32362f3cdd77
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-22 09:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const formElement = document.querySelector("#MyForm");
 function volume_sphere() {
     //Write your code here
 	const radius = Number(formElement.radius.value);
-	const volume = (4 * Math.PI * radius**3) / 3.0;
+	const volume = ((4 * Math.PI * radius**3) / 3.0).toFixed(4);
 	
 	formElement.volume.value= volume;
 		
